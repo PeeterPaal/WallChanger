0deaf76119d415f482a20d3594fd58a68ca6834c
HermanPalm <44435286+HermanPalm@users.noreply.github.com>
2018-12-13 14:56:40 +0200

    Add files via upload

--- WallChanger.py
+++ WallChanger.py
@@ -1,21 +1,20 @@
-import requests
-import ctypes
-import os
-import urllib
+from requests import get
+from ctypes import windll
+from os import getcwd, path
+from urllib import request
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
-#from tkinter import messagebox
 
 
 def WeatherData(city):
     #The API call is different for different accounts
-    responce = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=eb820c62182f83ffae8b2129055f70c8")
+    responce = get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=eb820c62182f83ffae8b2129055f70c8")
     converted = responce.json()
     return(converted)
 
 def GetResolution():
-    user32 = ctypes.windll.user32
+    user32 = windll.user32
     #The number 78 is for the screen width and 79 is for the height
     resolution = str(user32.GetSystemMetrics(78)) + "x" + str(user32.GetSystemMetrics(79))
     return(resolution)
@@ -26,34 +25,37 @@ def GetImg(key, resolution):
          500: "rain", 501: "rain", 502: "rain", 503: "rain", 504: "rain", 511: "rain", 520: "rain", 521: "rain", 522: "rain", 531: "rain",
          600: "snow", 601: "snow", 602: "snow", 611: "snow", 612: "snow", 615: "snow", 616: "snow", 620: "snow", 621: "snow", 622: "snow",
          701: "mist", 711: "smoke", 721: "haze", 731: "whirls", 741: "fog", 751: "sand", 761: "dust", 771: "squalls", 781: "tornado",
-         800: "clear", 801: "clouds", 802: "clouds", 803: "clouds", 804: "clouds"}
+         800: "sky", 801: "cloud", 802: "clouds", 803: "clouds", 804: "clouds"}
     weather = types[key]
     url = "https://source.unsplash.com/" + resolution + "/?" + weather
-    d = urllib.request.urlopen(url)
-    #print(d.info()["Content-Length"])
-    urllib.request.urlretrieve(url, "W.jpg")
+    request.urlretrieve(url, "W.jpg")
 
 def SetWallpaper():
-    location = os.getcwd()
-    path = os.path.normpath(location + "/W.jpg")
+    location = getcwd()
+    p = path.normpath(location + "/W.jpg")
     #The number 20 indicates the action of changing the wallpaper
     SPI_SET_WALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SET_WALLPAPER, 0, path, 0)
+    windll.user32.SystemParametersInfoW(SPI_SET_WALLPAPER, 0, p, 0)
+
 
 #city = input("City: ")
 
-
-#This is the position of the current weather info in the data
-def change():
+def change(event=None):
+    tekst.configure(text="Palun oota natukene")
+    tekst.update()
     city = nimi.get()
     data = WeatherData(city)
-    key = data["weather"][0]["id"]
-    resolution = GetResolution()
-    GetImg(key, resolution)
-    SetWallpaper()
-    raam.destroy()
-
-
+    #data["weather"][0]["id"] is the position of the current weather info in the data
+    if data["cod"] == "400":
+        tekst.configure(text="palun ikka sisestada linna nimi")
+    if data["cod"] == "404":
+        tekst.configure(text="väär linna nimi") 
+    if data["cod"] == 200:
+        tekst.configure(text="Vahetatud")
+        key = data["weather"][0]["id"]
+        resolution = GetResolution()
+        GetImg(key, resolution)
+        SetWallpaper()
 
 raam = Tk()
 raam.title("WallChanger")
@@ -77,6 +79,8 @@ rb = ImageTk.PhotoImage(Image.open("./rainbow_45.png"))
 sun = ImageTk.PhotoImage(Image.open("./sun_45.png"))
 Label(raam, image = rb).place(x = 10, y = 30, width = 50)
 Label(raam, image = sun).place(x = 240, y = 30)
+
+raam.bind('<Return>', change)
 
 raam.mainloop()
 
